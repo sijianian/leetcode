89bf539cb8afb96b717d25acc80907d3f1685701
moke <moke@xiaoman.cn>
2019-08-23 15:37:49 +0800

    feat: add tree

---
+++ 124.二叉树中的最大路径和.js
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=124 lang=javascript
+ *
+ * [124] 二叉树中的最大路径和
+ *
+ * https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/description/
+ *
+ * algorithms
+ * Hard (37.26%)
+ * Likes:    173
+ * Dislikes: 0
+ * Total Accepted:    12K
+ * Total Submissions: 32.2K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个非空二叉树，返回其最大路径和。
+ *
+ * 本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3]
+ *
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   3
+ *
+ * 输出: 6
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [-10,9,20,null,null,15,7]
+ *
+ * -10
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ *
+ * 输出: 42
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxPathSum = root => {
+  let max = -Number.MAX_VALUE
+
+  const getMaxSum = node => {
+    if (!node) {
+      return 0
+    }
+
+    const leftSum = getMaxSum(node.left)
+    const rightSum = getMaxSum(node.right)
+
+    max = Math.max(max, node.val + leftSum + rightSum)
+
+    return Math.max(0, node.val + leftSum, node.val + rightSum)
+  }
+
+  getMaxSum(root)
+
+  return max
+}
---
+++ 230.二叉搜索树中第k小的元素.js
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=javascript
+ *
+ * [230] 二叉搜索树中第K小的元素
+ *
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (65.91%)
+ * Likes:    89
+ * Dislikes: 0
+ * Total Accepted:    19.1K
+ * Total Submissions: 29.1K
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+ *
+ * 说明：
+ * 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+ *
+ * 示例 1:
+ *
+ * 输入: root = [3,1,4,null,2], k = 1
+ * ⁠  3
+ * ⁠ / \
+ * ⁠1   4
+ * ⁠ \
+ * 2
+ * 输出: 1
+ *
+ * 示例 2:
+ *
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ * ⁠      5
+ * ⁠     / \
+ * ⁠    3   6
+ * ⁠   / \
+ * ⁠  2   4
+ * ⁠ /
+ * ⁠1
+ * 输出: 3
+ *
+ * 进阶：
+ * 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+
+const kthSmallest = (root, k) => {
+  const arr = []
+  const stack = []
+
+  let current = root
+
+  while (current || stack.length > 0) {
+    while (current) {
+      stack.push(current)
+      current = current.left
+    }
+
+    current = stack.pop()
+    arr.push(current)
+    current = current.right
+  }
+
+  if (k > 0 && k <= arr.length) {
+    return arr[k - 1].val
+  }
+
+  return null
+}
