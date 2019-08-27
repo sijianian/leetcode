e03c64e5a56ccb149c2cb893fc5284d070249b8b
moke <moke@xiaoman.cn>
2019-08-27 16:52:40 +0800

    feat: add

---
+++ 88.合并两个有序数组.js
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=javascript
+ *
+ * [88] 合并两个有序数组
+ *
+ * https://leetcode-cn.com/problems/merge-sorted-array/description/
+ *
+ * algorithms
+ * Easy (45.06%)
+ * Likes:    299
+ * Dislikes: 0
+ * Total Accepted:    63.9K
+ * Total Submissions: 141.7K
+ * Testcase Example:  '[1,2,3,0,0,0]\n3\n[2,5,6]\n3'
+ *
+ * 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+ *
+ * 说明:
+ *
+ *
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+ * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ *
+ *
+ * 示例:
+ *
+ * 输入:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ *
+ * 输出: [1,2,2,3,5,6]
+ *
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  let j = 0
+
+  for (let i = 0; i < nums2.length; i++) {
+    while (!(nums2[i] < nums1[j] || j >= m + i)) {
+      j++
+    }
+
+    for (let k = nums1.length - 1; k > j; k--) {
+      nums1[k] = nums1[k - 1]
+    }
+
+    nums1[j] = nums2[i]
+  }
+}
