83ebd5a914b4d71696279aae0dfc0b6c6e120a67
moke <moke@xiaoman.cn>
2019-09-04 10:31:23 +0800

    fix: modify

--- 4.寻找两个有序数组的中位数.js
+++ 4.寻找两个有序数组的中位数.js
@@ -42,33 +42,10 @@
  * @return {number}
  */
 const findMedianSortedArrays = (nums1, nums2) => {
-  if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1)
+  const arr = [...nums1, ...nums2].sort((a, b) => a - b)
+  const { length } = arr
 
-  const x = nums1.length,
-    y = nums2.length
-  let lo = 0,
-    hi = x
-
-  while (lo <= hi) {
-    let partitionX = ((lo + hi) / 2) | 0,
-      partitionY = ((x + y + 1) / 2 - partitionX) | 0
-
-    let maxLeftX = partitionX === 0 ? -Infinity : nums1[partitionX - 1]
-    let minRightX = partitionX === x ? Infinity : nums1[partitionX]
-
-    let maxLeftY = partitionY === 0 ? -Infinity : nums2[partitionY - 1]
-    let minRightY = partitionY === y ? Infinity : nums2[partitionY]
-
-    if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-      if ((x + y) & 1) {
-        return Math.max(maxLeftX, maxLeftY)
-      }
-
-      return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2
-    } else if (maxLeftX > minRightY) {
-      hi = partitionX - 1
-    } else {
-      lo = partitionX + 1
-    }
-  }
+  return length % 2
+    ? arr[Math.floor(length / 2)]
+    : (arr[length / 2] + arr[length / 2 - 1]) / 2
 }
