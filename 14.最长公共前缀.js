d92a0bc868df144dd93fc34513256a35463cc3e5
moke <moke@xiaoman.cn>
2019-09-06 17:35:47 +0800

    fix: modify longest common prefix

--- 14.最长公共前缀.js
+++ 14.最长公共前缀.js
@@ -40,27 +40,17 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-  if (strs.length == 0) {
+  if (strs === undefined || strs.length === 0) {
     return ''
   }
 
-  let ans = strs[0]
+  return strs.reduce((prev, next) => {
+    let i = 0
 
-  for (let i = 1; i < strs.length; i++) {
-    let j = 0
-
-    for (; j < ans.length && j < strs[i].length; j++) {
-      if (ans[j] != strs[i][j]) {
-        break
-      }
+    while (prev[i] && next[i] && prev[i] === next[i]) {
+      i++
     }
 
-    ans = ans.substr(0, j)
-
-    if (ans === '') {
-      return ans
-    }
-  }
-
-  return ans
+    return prev.slice(0, i)
+  })
 }
