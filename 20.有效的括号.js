224f167f75fcf1c2f9e8d482982edbfe485e057f
moke <moke@xiaoman.cn>
2019-09-06 15:42:43 +0800

    feat: add isValid

---
+++ 20.有效的括号.js
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (39.32%)
+ * Likes:    1034
+ * Dislikes: 0
+ * Total Accepted:    118.4K
+ * Total Submissions: 299.6K
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ *
+ *
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: true
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: "()[]{}"
+ * 输出: true
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: "(]"
+ * 输出: false
+ *
+ *
+ * 示例 4:
+ *
+ * 输入: "([)]"
+ * 输出: false
+ *
+ *
+ * 示例 5:
+ *
+ * 输入: "{[]}"
+ * 输出: true
+ *
+ */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = s => {
+  const map = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  }
+  const stack = []
+
+  for (let str of s) {
+    if (str in map) {
+      stack.push(str)
+    } else {
+      if (str !== map[stack.pop()]) {
+        return false
+      }
+    }
+  }
+
+  return !stack.length
+}
