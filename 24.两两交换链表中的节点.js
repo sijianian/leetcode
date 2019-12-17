f7a0da509e8d72386d83b6710c75af7198158d81
moke <444295467@qq.com>
2019-12-17 20:16:07 +0800

    :art: feat: add swap-nodes-in-pairs

---
+++ 24.两两交换链表中的节点.js
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=javascript
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (63.12%)
+ * Likes:    357
+ * Dislikes: 0
+ * Total Accepted:    57.9K
+ * Total Submissions: 91.4K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ *
+ *
+ * 示例:
+ *
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const swapPairs = function(head) {
+  if (!head || !head.next) {
+    return head
+  }
+
+  let v1 = head
+  let v2 = head.next
+  let v3 = v2.next
+
+  v2.next = v1
+  v1.next = swapPairs(v3)
+
+  return v2
+}
+// @lc code=end
