375ca0ed03e1c1fe81cd65c1eee64d72909c3aa4
moke <444295467@qq.com>
2019-12-20 00:20:23 +0800

    :zap: fix: modify problem

---
+++ 279.完全平方数.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=279 lang=javascript
+ *
+ * [279] 完全平方数
+ *
+ * https://leetcode-cn.com/problems/perfect-squares/description/
+ *
+ * algorithms
+ * Medium (51.28%)
+ * Likes:    260
+ * Dislikes: 0
+ * Total Accepted:    29.3K
+ * Total Submissions: 56.5K
+ * Testcase Example:  '12'
+ *
+ * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+ *
+ * 示例 1:
+ *
+ * 输入: n = 12
+ * 输出: 3
+ * 解释: 12 = 4 + 4 + 4.
+ *
+ * 示例 2:
+ *
+ * 输入: n = 13
+ * 输出: 2
+ * 解释: 13 = 4 + 9.
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function(n) {}
+// @lc code=end
