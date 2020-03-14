5e93a9cec5a00b67939152956e34376b0fc5413b
moke <444295467@qq.com>
2020-03-14 22:15:45 +0800

    :art: feat: add 153 162 278

---
+++ 153.寻找旋转排序数组中的最小值.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=153 lang=javascript
+ *
+ * [153] 寻找旋转排序数组中的最小值
+ *
+ * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (49.96%)
+ * Likes:    149
+ * Dislikes: 0
+ * Total Accepted:    36.2K
+ * Total Submissions: 72.2K
+ * Testcase Example:  '[3,4,5,1,2]'
+ *
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ *
+ * ( 例如，数组 [0,1,2,4,5,6,7]  可能变为 [4,5,6,7,0,1,2] )。
+ *
+ * 请找出其中最小的元素。
+ *
+ * 你可以假设数组中不存在重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,4,5,1,2]
+ * 输出: 1
+ *
+ * 示例 2:
+ *
+ * 输入: [4,5,6,7,0,1,2]
+ * 输出: 0
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMin = function(nums) {
+  // let left = 0
+  // let right = nums.length - 1
+
+  // while (left < right) {
+  //   let mid = Math.floor(left + ((right - left) >> 1))
+
+  //   if (nums[left] <= nums[mid]) {
+  //     left = mid + 1
+  //   } else {
+  //     right = mid
+  //   }
+  // }
+
+  // return nums[left]
+  let l = 0
+  let r = nums.length - 1
+
+  while (l < r) {
+    const m = Math.floor((l + r) / 2)
+
+    if (nums[m] > nums[r]) {
+      l = m + 1
+    } else {
+      r = m
+    }
+  }
+  return nums[l]
+}
+// @lc code=end
---
+++ 278.第一个错误的版本.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=278 lang=javascript
+ *
+ * [278] 第一个错误的版本
+ *
+ * https://leetcode-cn.com/problems/first-bad-version/description/
+ *
+ * algorithms
+ * Easy (34.99%)
+ * Likes:    144
+ * Dislikes: 0
+ * Total Accepted:    39.1K
+ * Total Submissions: 104.8K
+ * Testcase Example:  '5\n4'
+ *
+ *
+ * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+ *
+ * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+ *
+ * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version
+ * 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ *
+ * 示例:
+ *
+ * 给定 n = 5，并且 version = 4 是第一个错误的版本。
+ *
+ * 调用 isBadVersion(3) -> false
+ * 调用 isBadVersion(5) -> true
+ * 调用 isBadVersion(4) -> true
+ *
+ * 所以，4 是第一个错误的版本。
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+const solution = function(isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+    let left = 1
+    let right = n
+
+    while (left < right) {
+      let mid = left + ((right - left) >> 1)
+
+      if (isBadVersion(mid)) {
+        right = mid
+      } else {
+        left = mid + 1
+      }
+    }
+
+    return left
+  }
+}
+// @lc code=end
