a26146b827798b795367543003a6c90a65e668fc
moke <444295467@qq.com>
2020-03-14 18:00:10 +0800

    :art: feat: add 374 704

---
+++ 374.猜数字大小.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=374 lang=javascript
+ *
+ * [374] 猜数字大小
+ *
+ * https://leetcode-cn.com/problems/guess-number-higher-or-lower/description/
+ *
+ * algorithms
+ * Easy (41.46%)
+ * Likes:    54
+ * Dislikes: 0
+ * Total Accepted:    18K
+ * Total Submissions: 41.7K
+ * Testcase Example:  '10\n6'
+ *
+ * 我们正在玩一个猜数字游戏。 游戏规则如下：
+ * 我从 1 到 n 选择一个数字。 你需要猜我选择了哪个数字。
+ * 每次你猜错了，我会告诉你这个数字是大了还是小了。
+ * 你调用一个预先定义好的接口 guess(int num)，它会返回 3 个可能的结果（-1，1 或 0）：
+ *
+ * -1 : 我的数字比较小
+ * ⁠1 : 我的数字比较大
+ * ⁠0 : 恭喜！你猜对了！
+ *
+ *
+ * 示例 :
+ *
+ * 输入: n = 10, pick = 6
+ * 输出: 6
+ *
+ */
+
+// @lc code=start
+
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const guessNumber = function(n) {
+  let left = 1
+  let right = n
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2)
+    let res = guess(mid)
+
+    if (res === 0) {
+      return mid
+    } else if (res < 0) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+
+  return -1
+}
+// @lc code=end
---
+++ 704.二分查找.js
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=javascript
+ *
+ * [704] 二分查找
+ *
+ * https://leetcode-cn.com/problems/binary-search/description/
+ *
+ * algorithms
+ * Easy (51.33%)
+ * Likes:    90
+ * Dislikes: 0
+ * Total Accepted:    23.8K
+ * Total Submissions: 46.3K
+ * Testcase Example:  '[-1,0,3,5,9,12]\n9'
+ *
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的
+ * target，如果目标值存在返回下标，否则返回 -1。
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [-1,0,3,5,9,12], target = 9
+ * 输出: 4
+ * 解释: 9 出现在 nums 中并且下标为 4
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: nums = [-1,0,3,5,9,12], target = 2
+ * 输出: -1
+ * 解释: 2 不存在 nums 中因此返回 -1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 你可以假设 nums 中的所有元素是不重复的。
+ * n 将在 [1, 10000]之间。
+ * nums 的每个元素都将在 [-9999, 9999]之间。
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = function(nums, target) {
+  let pivot
+  let left = 0
+  let right = nums.length
+
+  while (left <= right) {
+    pivot = Math.floor(((right - left) >> 1) + left)
+
+    if (nums[pivot] === target) {
+      return pivot
+    } else if (target < nums[pivot]) {
+      right = pivot - 1
+    } else {
+      left = pivot + 1
+    }
+  }
+
+  return -1
+}
+// @lc code=end
