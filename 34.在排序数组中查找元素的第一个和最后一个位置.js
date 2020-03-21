11d6ad4f274ae67fececdfc4fb94315c39db1957
moke <444295467@qq.com>
2020-03-21 16:21:25 +0800

    :art: feat: add 34 658

---
+++ 34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=javascript
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (38.15%)
+ * Likes:    343
+ * Dislikes: 0
+ * Total Accepted:    71.2K
+ * Total Submissions: 181.7K
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ *
+ * 如果数组中不存在目标值，返回 [-1, -1]。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [5,7,7,8,8,10], target = 8
+ * 输出: [3,4]
+ *
+ * 示例 2:
+ *
+ * 输入: nums = [5,7,7,8,8,10], target = 6
+ * 输出: [-1,-1]
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const searchRange = function(nums, target) {
+  const targetRange = [-1, -1]
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      targetRange[0] = i
+      break
+    }
+  }
+
+  if (targetRange[0] === -1) {
+    return targetRange
+  }
+
+  for (let j = nums.length - 1; j >= 0; j--) {
+    if (nums[j] === target) {
+      targetRange[1] = j
+      break
+    }
+  }
+
+  return targetRange
+}
+// @lc code=end
---
+++ 658.找到-k-个最接近的元素.js
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=658 lang=javascript
+ *
+ * [658] 找到 K 个最接近的元素
+ *
+ * https://leetcode-cn.com/problems/find-k-closest-elements/description/
+ *
+ * algorithms
+ * Medium (41.74%)
+ * Likes:    65
+ * Dislikes: 0
+ * Total Accepted:    7.1K
+ * Total Submissions: 16.7K
+ * Testcase Example:  '[1,2,3,4,5]\n4\n3'
+ *
+ * 给定一个排序好的数组，两个整数 k 和 x，从数组中找到最靠近 x（两数之差最小）的 k 个数。返回的结果必须要是按升序排好的。如果有两个数与 x
+ * 的差值一样，优先选择数值较小的那个数。
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: [1,2,3,4,5], k=4, x=3
+ * 输出: [1,2,3,4]
+ *
+ *
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: [1,2,3,4,5], k=4, x=-1
+ * 输出: [1,2,3,4]
+ *
+ *
+ *
+ *
+ * 说明:
+ *
+ *
+ * k 的值为正数，且总是小于给定排序数组的长度。
+ * 数组不为空，且长度不超过 10^4
+ * 数组里的每个元素与 x 的绝对值不超过 10^4
+ *
+ *
+ *
+ *
+ * 更新(2017/9/19):
+ * 这个参数 arr 已经被改变为一个整数数组（而不是整数列表）。 请重新加载代码定义以获取最新更改。
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+
+// TODO:
+const findClosestElements = function(arr, k, x) {
+  arr = arr.sort((a, b) => a - b)
+}
+// @lc code=end
