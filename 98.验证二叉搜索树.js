9c900785ff4c48918d5d4afae4a6538f694300b3
moke <444295467@qq.com>
2020-04-04 17:04:57 +0800

    :art: feat: add 98.isValidBST

---
+++ 98.验证二叉搜索树.js
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=javascript
+ *
+ * [98] 验证二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (27.90%)
+ * Likes:    484
+ * Dislikes: 0
+ * Total Accepted:    86.7K
+ * Total Submissions: 292.8K
+ * Testcase Example:  '[2,1,3]'
+ *
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ *
+ * 假设一个二叉搜索树具有如下特征：
+ *
+ *
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ *
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ * 输出: true
+ *
+ *
+ * 示例 2:
+ *
+ * 输入:
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ * 根节点的值为 5 ，但是其右子节点值为 4 。
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isValidBST = function (root) {
+  const stack = []
+  let current = root
+  let lastNode = ''
+
+  while (current !== null || stack.length !== 0) {
+    while (current !== null) {
+      stack.push(current)
+      current = current.left
+    }
+
+    current = stack.pop()
+
+    if (lastNode !== '' && current.val <= lastNode) {
+      return false
+    }
+
+    lastNode = current.val
+    current = current.right
+  }
+
+  return true
+}
+// @lc code=end
